ab20c80b5596b7340acca6864a65ee841c9b7aea
hsm <1161237802@qq.com>
2025-12-09 15:36:02 +0800

    login done

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import Homepage from "./pages/HomePage";
 import Login from "./pages/Login";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
+import AppLayout from "./pages/AppLayout";
+import { AuthProvider } from "./contexts/FakeAuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/product" element={<Product />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/app" element={<AppLayout />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,60 @@
+import { useEffect, useState } from "react";
+import PageNav from "../components/PageNav";
+import styles from "./Login.module.css";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router";
+import toast from "react-hot-toast";
+import Button from "../contexts/Button";
+
 function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("HSM@example.com");
+  const [password, setPassword] = useState("12345678");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated]
+  );
+
+  return (
+    <main className={styles.login}>
+      <PageNav />
+
+      <form className={styles.form} onSubmit={(e) => handleSubmit(e)}>
+        <div className={styles.row}>
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+
+        <div className={styles.row}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <div>
+          <Button type="primary">Login</Button>
+        </div>
+      </form>
+    </main>
+  );
 }
 
 export default Login;
